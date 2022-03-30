fc72181d35135e89dde161a0884b6e89a146b7cf
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2022-03-30 10:57:15 +0300

    As a user I don't want to see the error message in console about tax calculation [#181695583]

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 onchange = function () { calcPrice };
 
-
 let stateTax = [
     { code: "UT", tax: 1.05 },
     { code: "NV", tax: 1.08 },
@@ -15,54 +14,56 @@ function calcPrice(event) {
     const stateCode = document.querySelector("#stateInput").value;
     const totalPrice = document.querySelector("#totalPrc");
 
-    let orderValue = amountItems * priceItem;
-    let startPrice = orderValue;
-    let toDiscount
+    if (amountItems && priceItem && stateCode) {
+        let orderValue = amountItems * priceItem;
+        let startPrice = orderValue;
+        let toDiscount
 
-    if (orderValue < 1000) {
-        toDiscount = 1000 - orderValue;
-    }
+        if (orderValue < 1000) {
+            toDiscount = 1000 - orderValue;
+        }
 
-    if (orderValue >= 1000 && orderValue < 5000) {
-        orderValue = orderValue * 0.97;
-        toDiscount = 5000 - startPrice;
-    }
-    if (orderValue >= 5000 && orderValue < 7000) {
-        orderValue = orderValue * 0.95;
-        toDiscount = 7000 - startPrice;
-    }
-    if (orderValue >= 7000 && orderValue < 10000) {
-        orderValue = orderValue * 0.93;
-        toDiscount = 10000 - startPrice;
-    }
-    if (orderValue >= 10000 && orderValue < 50000) {
-        orderValue = orderValue * 0.9;
-        toDiscount = 50000 - startPrice;
-    }
-    if (orderValue >= 50000) {
-        orderValue = orderValue * 0.85;
-    }
+        if (orderValue >= 1000 && orderValue < 5000) {
+            orderValue = orderValue * 0.97;
+            toDiscount = 5000 - startPrice;
+        }
+        if (orderValue >= 5000 && orderValue < 7000) {
+            orderValue = orderValue * 0.95;
+            toDiscount = 7000 - startPrice;
+        }
+        if (orderValue >= 7000 && orderValue < 10000) {
+            orderValue = orderValue * 0.93;
+            toDiscount = 10000 - startPrice;
+        }
+        if (orderValue >= 10000 && orderValue < 50000) {
+            orderValue = orderValue * 0.9;
+            toDiscount = 50000 - startPrice;
+        }
+        if (orderValue >= 50000) {
+            orderValue = orderValue * 0.85;
+        }
 
-    const StateTax = Object.keys(stateTax).map(k => stateTax[k])[stateCode].tax
+        StateTax = Object.keys(stateTax).map(k => stateTax[k])[stateCode].tax
 
-    let valueWithTax = orderValue * StateTax;
+        let valueWithTax = orderValue * StateTax;
 
-    if (valueWithTax === 0) {
-        totalPrice.textContent = "Total Price: "
-    } else {
-        totalPrice.textContent = "Total Price: $" + (Math.round(valueWithTax * 100) / 100);
-    }
+        if (valueWithTax === 0) {
+            totalPrice.textContent = "Total Price: "
+        } else {
+            totalPrice.textContent = "Total Price: $" + (Math.round(valueWithTax * 100) / 100);
+        }
 
-    let savedSum = startPrice - orderValue
-    if (savedSum === 0) {
-        totalSave.textContent = "You saved: "
-    } else {
-        totalSave.textContent = "You saved: $" + (Math.round(savedSum * 100) / 100);
-    }
+        let savedSum = startPrice - orderValue
+        if (savedSum === 0) {
+            totalSave.textContent = "You saved: $0"
+        } else {
+            totalSave.textContent = "You saved: $" + (Math.round(savedSum * 100) / 100);
+        }
 
-    if (toDiscount >= 1000 && toDiscount < 50000) {
-        totalMore.textContent = "You need to spend $" + toDiscount + " more for a better discount";
-    } else {
-        totalMore.textContent = "You need to spend $" + toDiscount + " more for a discount";
-    }
+        if (toDiscount >= 1000 && toDiscount < 50000) {
+            totalMore.textContent = "You need to spend $" + toDiscount + " more for a better discount";
+        } else {
+            totalMore.textContent = "You need to spend $" + toDiscount + " more for a discount";
+        }
+    }    
 }
